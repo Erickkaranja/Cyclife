1f003478e686294e3cbe1a3374fdc39eabd27039
Blackbeing <johnjahi55@gmail.com>
2023-08-31 13:07:10 +0300

    refactor Replace cmd with cmd2
      cmd2 offers argparse capabilities. This avoids parsing args manually
      Add console option to add user

--- console.py
+++ console.py
@@ -2,53 +2,93 @@
 """a simple cmd which will be used by admin to add products,
    update products and delete products.
 """
-import cmd
+import sys
 
-from models.bicycle import Bicycle
+import cmd2
+
+from models.user import User
+
+base_parser = cmd2.Cmd2ArgumentParser(
+    description="Cyclife CLI",
+)
+base_subparsers = base_parser.add_subparsers(
+    title="subcommands", help="subcommand help"
+)
+create_parser = base_subparsers.add_parser("create", help="create object")
+create_subparsers = create_parser.add_subparsers(
+    title="subcommands", help="subcommand help"
+)
+create_user_parser = create_subparsers.add_parser("user", help="create user")
+create_user_parser.add_argument("-f", "--first_name", help="user first name")
+create_user_parser.add_argument("-l", "--last_name", help="user last name")
+create_user_parser.add_argument("-e", "--email", help="user email")
+create_user_parser.add_argument("-p", "--password", help="user password")
 
 
-class CyclifeCommand(cmd.Cmd):
+class CyclifeCommand(cmd2.Cmd):
     """Cyclife console."""
 
     prompt = "(Cyclife) "
-
-    def do_EOF(self, arg):
-        """exit console."""
-        return True
 
     def emptyline(self):
         """overrite empty line method."""
         return False
 
-    def do_quit(self, arg):
-        """quit command to exit the console."""
+    def do_exit(self, arg):
+        """Exit"""
+        self.poutput("\n** Exiting program, bye **")
         return True
 
-    def do_create(self, args):
-        """create's a new instance of our product bicycle.
-        usage: create bicycle <key1=value1> <key2=value2> <key3=value3> ...
-        prints id after new instance is created.
+    def do_quit(self, arg):
+        """Exit"""
+        self.poutput("\n** Exiting program, bye **")
+        return True
+
+    def do_EOF(self, arg):
         """
-        new_dict = {}
-        my_list = args.split(" ")
-        for arg in my_list[1:]:
-            if "=" in arg:
-                kvp = arg.split("=", 1)
-                key = kvp[0]
-                value = kvp[1]
-                if "_" in value:
-                    value = value.replace("_", " ")
-                elif "_" not in value:
-                    value = str(value)
-                else:
-                    value = int(value)
-            new_dict[key] = value
-        obj = Bicycle(**new_dict)
-        print(new_dict)
-        print(type(new_dict))
-        print(obj.id)
-        obj.save()
+        Exit
+        """
+        self.poutput("\n** Exiting program, bye **")
+        return True
+
+    def postloop(self):
+        self.poutput()
+
+    def create_user(self, args):
+        """
+        Create new instance of user object
+        """
+        new_dict = {
+            k: v
+            for k, v in dict(args._get_kwargs()).items()
+            if k in ["first_name", "last_name", "email", "password"]
+        }
+        new_user = User(**new_dict)
+        print(new_user.id)
+        new_user.save()
+
+    create_user_parser.set_defaults(func=create_user)
+
+    @cmd2.with_argparser(create_parser)
+    def do_create(self, args):
+        """
+        Add command help
+        """
+        func = getattr(args, "func", None)
+        if func is not None:
+            func(self, args)
+        else:
+            self.do_help("base")
+
+
+def main():
+    cyclifecmd = CyclifeCommand()
+    try:
+        sys.exit(cyclifecmd.cmdloop())
+    except (KeyboardInterrupt, EOFError):
+        cyclifecmd.poutput("\nExiting the program\n")
+    return True
 
 
 if __name__ == "__main__":
-    CyclifeCommand().cmdloop()
+    main()
