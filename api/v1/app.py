d25f4851c99c2e864ffa445916773efabf2c56a4
Blackbeing <johnjahi55@gmail.com>
2023-09-06 18:25:48 +0300

    refactor Import all views in init file

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,8 +5,21 @@ from flask import Flask
 from api.v1.views import app_views
 from models import storage
 
-app = Flask(__name__)
-app.register_blueprint(app_views)
+
+def create_app():
+    """
+    Factory for the application
+
+    Returns:
+        Instance of flask app
+    """
+
+    app = Flask(__name__)
+    app.register_blueprint(app_views)
+    return app
+
+
+app = create_app()
 
 
 @app.teardown_appcontext
